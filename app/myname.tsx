0fa52c9472ad0aa0fae699460659d980b7a421e7
MelodyHuang3073 <y920531@gmail.com>
2023-09-20 18:41:37 +0800

    0920 home review

--- app/myname.tsx
+++ app/myname.tsx
@@ -1,8 +1,7 @@
-  export default function MyName(){
-    return(
-        <div>
-            <h1>Hello</h1>
-            <h2>我是美樂蒂</h2>
-        </div>
-    )
-  }
+export default function MyName() {
+  return (
+    <div>
+      <h2>我是美樂蒂</h2>
+    </div>
+  );
+}
