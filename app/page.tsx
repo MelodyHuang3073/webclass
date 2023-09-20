746f6ab9a0565bba6f7194f7ab3e2e990f520cd3
MelodyHuang3073 <y920531@gmail.com>
2023-09-20 18:40:04 +0800

    0920 homereview

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import MyName from './myname'
-import MyButton from './mybutton'
-import styles from './page.module.css'
+import MyName from "./myname";
+import MyButton from "./mybutton";
+import styles from "./page.module.css";
 
 export default function Home() {
-  return (<div className={styles.main}>
-    <MyName/>
-    <MyButton/>
-    </div>)
+  return (
+    <div className={styles.main}>
+      <h1>Hello</h1>
+      <MyName />
+      <MyButton />
+    </div>
+  );
 }
