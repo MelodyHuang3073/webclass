4f8a11d4c1e6e4dff03d3346c776a0e8ceade4ab
MelodyHuang3073 <y920531@gmail.com>
2023-10-01 15:59:49 +0800

    1001hometesting

--- app/listitem.tsx
+++ app/listitem.tsx
@@ -1,20 +1,20 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import { Box, List, ListItem, ListItemText } from "@mui/material";
-import ListItemButton from '@mui/material/ListItemButton';
+import ListItemButton from "@mui/material/ListItemButton";
 
 export default function ProductList() {
   const products = [
     { desc: "iPad", price: 20000, index: 0 },
     { desc: "iPhone 8", price: 20000, index: 1 },
-    { desc: "iPhone X", price: 30000, index: 2 }
+    { desc: "iPhone X", price: 30000, index: 2 },
   ];
 
   const [selectedIndex, setSelectedIndex] = React.useState(1);
 
   const handleListItemClick = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-    index: number,
+    index: number
   ) => {
     setSelectedIndex(index);
   };
@@ -22,25 +22,25 @@ export default function ProductList() {
   return (
     <Box
       sx={{
-        width: '80vw',
-        height: '100vh',
-        backgroundColor: 'background.paper',
-        color: 'black',
-        textAlign: 'left'
+        width: "80vw",
+        height: "100vh",
+        backgroundColor: "background.paper",
+        color: "black",
+        textAlign: "left",
       }}
     >
       <List subheader="Product list" aria-label="product list">
         {products.map((product) => (
-          <ListItem
-            divider
-            key={product.desc}
-          >
-          <ListItemButton
-          selected ={selectedIndex === product.index}
-          onClick={(event) => handleListItemClick(event, product.index)}
-          >
-          <ListItemText primary={product.desc} secondary={`Price: ${product.price}`} />
-          </ListItemButton>
+          <ListItem divider key={product.desc}>
+            <ListItemButton
+              selected={selectedIndex === product.index}
+              onClick={(event) => handleListItemClick(event, product.index)}
+            >
+              <ListItemText
+                primary={product.desc}
+                secondary={`Price: ${product.price}`}
+              />
+            </ListItemButton>
           </ListItem>
         ))}
       </List>
