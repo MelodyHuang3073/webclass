83eddbca7a9a5998aef6d70dde5d7477aa74d241
MelodyHuang3073 <y920531@gmail.com>
2023-10-18 12:14:39 +0800

    20231018

---
+++ app/_firebase/Config.tsx
@@ -0,0 +1,15 @@
+import { getApps, initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCLwnmd4ZBfi0fYA7AtVHLcfZyh486slwU",
+    authDomain: "qampus-7b72a.firebaseapp.com",
+    projectId: "qampus-7b72a",
+    storageBucket: "qampus-7b72a.appspot.com",
+    messagingSenderId: "950103232939",
+    appId: "1:950103232939:web:bc1b5b5378c4dd4aa402ee",
+    measurementId: "G-06TQLJFSE9"
+}
+// Initialize Firebase App
+let firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+export default firebase_app;
--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,7 +1,18 @@
-import ProductList from '../product'
+"use client"
+import useRead from '../read'
 
 export default function Home() {
-  return (<div>
-    <ProductList />
-  </div>)
+  const [products, setProducts]=useRead();
+  return (
+    <div>
+      <h1>Product List</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            <strong>Desc:</strong> {product.desc}, <strong>Price:</strong> {product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
