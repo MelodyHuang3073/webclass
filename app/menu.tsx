d8c44ca8c3bcefaee37670c6d32afb7866efc03a
MelodyHuang <101505343+MelodyHuang3073@users.noreply.github.com>
2023-12-13 10:38:10 +0800

    Update menu.tsx

--- app/menu.tsx
+++ app/menu.tsx
@@ -14,7 +14,6 @@ export default function Menu() {
       <Toolbar>
         <Button color="inherit" variant={pathname === "/account" ? "outlined" : "text"} onClick={() => router.push("/account")}>主頁面</Button>
         <Button color="inherit" variant={pathname === "/product" ? "outlined" : "text"} onClick={() => router.push("/product")}>QA管理</Button>
-        <Button color="inherit" variant={pathname === "/admin" ? "outlined" : "text"} onClick={() => router.push("/admin")}>權限管理</Button>
         {authContext}
       </Toolbar>
     </AppBar>
